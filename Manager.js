af490b5fbf1bf31683af177e8c30df8effcce449
NordTort <41715818+NorthTort@users.noreply.github.com>
2020-06-08 18:31:32 +0300

    Update Manager.js

--- Manager.js
+++ Manager.js
@@ -11,14 +11,13 @@ var prefix = '+'
  client.on('message', message => {
     if(message.author === client.user) return;
     
-	 if(message.content.startsWith(prefix + 'say')) {
-  if(message.author.id !== '719569602753069178') return;
-    const args = message.content.slice(prefix.length).split('say'); 
+	  if(message.content.startsWith(prefix + 'say')) {
+  if(message.author.id !== '386834372466049024') return;
+    const args = message.content.slice(prefix.length).split('say');
 	  message.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-  .catch(console.error); 
+  .catch(console.error);
      		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
-
 	 }
   	 });
 
