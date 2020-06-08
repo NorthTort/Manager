160c8a1e738cd61b0f54eb6c64ee492c32db8d63
NordTort <41715818+NorthTort@users.noreply.github.com>
2020-06-08 16:59:52 +0300

    Create Manager.js

---
+++ Manager.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+var prefix = '+'
+ 
+ client.on('message', message => {
+    if(message.author === client.user) return;
+    
+	 if(message.content.startsWith(prefix + 'say')) {
+  if(message.author.id !== '611875493515493378') return;
+    const args = message.content.slice(prefix.length).split('say'); 
+	  message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
+
+	 }
+  	 });
+
+client.login(process.env.BOT_TOKEN);
